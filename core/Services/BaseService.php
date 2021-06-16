033876a574ae748059674996f9825f9ec62f194a
tqcaoson <57176427+tqcaoson@users.noreply.github.com>
2021-06-16 15:57:35 +0700

    add transaction

--- core/Services/BaseService.php
+++ core/Services/BaseService.php
@@ -2,6 +2,8 @@
 
 namespace Core\Services;
 use Core\Services\ServiceInterface;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class BaseService implements ServiceInterface
 {
@@ -19,16 +21,52 @@ class BaseService implements ServiceInterface
 
     public function store($data)
     {
-        return $this->repository->store($data);
+        DB::beginTransaction();
+
+        try {
+            $repository = $this->repository->store($data);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::info($e->getMessage());
+            throw new InvalidArgumentException('Unable to delete post data');
+        }
+
+        DB::commit();
+
+        return $repository;
     }
 
     public function update($id, $data)
     {
-        return $this->repository->update($id, $data);
+        DB::beginTransaction();
+
+        try {
+            $repository = $this->repository->update($id, $data);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::info($e->getMessage());
+            throw new InvalidArgumentException('Unable to delete post data');
+        }
+
+        DB::commit();
+
+        return $repository;
     }
 
     public function destroy($id)
     {
-        return $this->repository->destroy($id);
+        DB::beginTransaction();
+
+        try {
+            $repository = $this->repository->destroy($id);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::info($e->getMessage());
+            throw new InvalidArgumentException('Unable to delete post data');
+        }
+
+        DB::commit();
+        
+        return $repository;
     }
 }
